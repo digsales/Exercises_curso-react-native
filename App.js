be11a5147e3bbbb9c366abd6bba7ebdc841d5731
_digsales <diogodobu@gmail.com>
2022-09-13 14:00:54 -0300

    testando

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-import Primeiro from './src/components/Primeiro';
-import CompPadrao, { Comp1, Comp2 } from './src/components/Multi';
-
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
 
-      <Primeiro />
-
-      <CompPadrao />
-      <Comp1 />
-      <Comp2 />
+      <Text>Primeiro app</Text>
       
       <StatusBar style="auto" />
 
--- src/components/Primeiro.js
+++
@@ -1,5 +0,0 @@
-import { Text } from "react-native";
-
-import Estilo from './estilo'
-
-export default () => <Text style={Estilo.fontG}>Primeiro Componente!!!</Text>
